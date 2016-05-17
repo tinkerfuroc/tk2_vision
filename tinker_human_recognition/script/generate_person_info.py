6417460e7c4fadd5c56ae2c4b566d984f1454bc8
gjc13 <amtcmp@126.com>
2016-05-17 18:39:42 +0800

    [Add] Human recognition package

---
+++ tinker_human_recognition/script/generate_person_info.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+from k2_client.msg._BodyArray import BodyArray
+from k2_client.msg._JointPositionAndState import JointPositionAndState
+from tinker_vision_msgs.msg._TrackPeople import TrackPeople
+from tinker_vision_msgs.msg._TrackPerson import TrackPerson
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+import rospy
+import numpy
+import cv2
+import sys
+from cv_bridge import CvBridge, CvBridgeError
+from threading import Lock
+
+def average_position(body):
+    sum_x = 0.
+    sum_y = 0.
+    sum_z = 0.
+    for joint in body.jointPositions:
+        sum_x += joint.position.x
+        sum_y += joint.position.y
+        sum_z += joint.position.z
+    point = Point(
+            sum_x / len(body.jointPositions),
+            sum_y / len(body.jointPositions),
+            sum_z / len(body.jointPositions)
+            )
+    
+
+class PersonInfoNode:
+    def __init__(self):
+        self._people_pub = rospy.Publisher('track_people', TrackPeople, queue_size=10)
+        self._image_pub = rospy.Publisher('~/people_track_img', Image, queue_size=10)
+        self._new_body = False
+        self._new_image = False
+        self._body_lock = Lock()
+        self._img_lock = Lock()
+        self.bodies = None
+        self.img = None
+
+
+    def body_handler(self, body_array):
+        with self._body_lock:
+            self.bodies = body_array.bodies
+            self._new_body = True
+
+    def image_handler(self, img):
+        with self._img_lock:
+            try:
+                self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            except CvBridgeError as e:
+                rospy.logwarn(e)
+            self._new_image = True
+
+    def pub_msgs(self):
+        with self._body_lock:
+            with self._img_lock:
+                if not self._new_body or not self._new_image:
+                    return
+                self._new_body = False
+                self._new_image = False
+                track_people = TrackPeople()
+                for body in self.bodies:
+                    person = TrackPerson()
+                    person.trackingId = body.trackingId
+                    person.pos = average_position(body)
+                    person.header = body.header
+                    cv2.rectangle(self.img, 
+                            (body.fromX, body.fromY), 
+                            (body.toX, body.toY),
+                            (255, 0, 0))
+                    track_people.people.append(person)
+                self._image_pub.publish(
+                        bridge.cv2_to_imgmsg(self.img, 'bgr8'))
+                self._people_pub.publish(track_people)
+
+
+def main(argv):
+    person_info_node = PersonInfoNode()
+    rospy.Subscriber('/bodyArray', BodyArray, person_info_node.body_handler)
+    rospy.Subscriber('/head/kinect2/rgb/image', Image, person_info_node.image_handler)
+    rate = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        person_info_node.pub_msgs()
+        rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('generate_person_info', anonymous=False)
+    main(sys.argv)
